736744b8c0283eb8418a3f321291143e6c6f9f8d
DeNiCoN <denicon1234@gmail.com>
2022-04-05 00:22:00 +0300

    Cardano add passphrase support

--- kaibamultipay/modules/cardano.py
+++ kaibamultipay/modules/cardano.py
@@ -1,5 +1,6 @@
 from kaibamultipay.modules.base import Module
 from kaibamultipay.errors import *
+import cardano.numbers
 from cardano.wallet import Wallet
 from cardano.backends.walletrest import WalletREST
 
@@ -10,7 +11,8 @@ class CardanoModule(Module):
     _wallet: Wallet
     _currency_name: str
 
-    def __init__(self, currency_name, wallet_id: str, host, port) -> None:
+    def __init__(self, currency_name, wallet_id: str, host='localhost', port=8090, passphrase=None) -> None:
+        self._passphrase = passphrase
         self._wallet = Wallet(wallet_id, backend=WalletREST(port=port, host=host))               
         self._currency_name = currency_name
 
@@ -22,7 +24,11 @@ class CardanoModule(Module):
             raise NoSuchCurrencyError(currency)
 
     def _send_native(self, address, amount):
-        tx = self._wallet.transfer(address, amount)
+        logger.debug(f"Cardano wallet sync proggress: {self._wallet.sync_progress()}")
+        logger.debug(f"Cardano wallet balance: {self._wallet.balance()}")
+
+        tx = self._wallet.transfer(address, cardano.numbers.from_lovelaces(
+            amount), passphrase=self._passphrase)
         logger.info(f"Cardano sent: {str(tx.txid)}")
         return tx.txid
 
@@ -32,10 +38,11 @@ class CardanoModule(Module):
             host = config.get("host", "localhost")
             port = config.get("port", 8090)
             native = config.get("native", "ADA")
+            passphrase = config.get("passphrase")
             wallet_id = config["wallet_id"]
         except KeyError as e:
             raise ConfigParseError(f"{e} is required in Cardano module config") from e
 
-        result = CardanoModule(native, wallet_id, host, port)
+        result = CardanoModule(native, wallet_id, host, port, passphrase=passphrase)
 
         return result
